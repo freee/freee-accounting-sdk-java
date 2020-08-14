6e724fc3a351b33762b72f477c6f39e8035c319d
Kazumi OHIRA <dzeyelid@gmail.com>
2020-08-14 04:48:24 +0000

    Fix missing import

--- samples/basic-websample/src/main/java/freee/accounting/samples/basic/web/controller/FreeeController.java
+++ samples/basic-websample/src/main/java/freee/accounting/samples/basic/web/controller/FreeeController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 import freee.accounting.samples.basic.web.services.AccountingService;
